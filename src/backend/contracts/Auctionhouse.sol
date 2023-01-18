7c40fd16c6c71b4df8cf5b3de48851edcea41cc8
Antonis Koufis <antokoufis@gmail.com>
2023-01-18 10:34:40 +0200

    Creates Item data struct and mapping them

--- src/backend/contracts/Auctionhouse.sol
+++ src/backend/contracts/Auctionhouse.sol
@@ -10,6 +10,20 @@ contract Auctionhouse is ReentrancyGuard {
     address payable public immutable feeAccount;
     // the fee percentage on sales
     uint256 public immutable feePercent;
+    // structs counters
+    uint256 public itemCount;
+
+    // Item struct
+    struct Item {
+        uint256 itemId;
+        IERC721 nft;
+        uint256 tokenId;
+        address payable owner;
+        uint256 status; //1 = Available for Auction, 2 =  NOT available for Auction Item
+    }
+
+    // itemId -> Item
+    mapping(uint => Item) public items;
 
     constructor(uint256 _feePercent) {
         feeAccount = payable(msg.sender);
